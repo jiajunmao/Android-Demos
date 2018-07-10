95b386d93e0a8923c98d6d86d0f7ee214f8b9eeb
jiajunmao <aaronmao.hangzhou@gmail.com>
2018-07-09 21:01:39 -0400

    Book 3.2.2 completed. Pop up window OK.

--- app/src/main/java/com/example/aaronmao/blindnav/MainActivity.java
+++ app/src/main/java/com/example/aaronmao/blindnav/MainActivity.java
@@ -1,13 +1,65 @@
 package com.example.aaronmao.blindnav;
 
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    public TextView text1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Open a window on click
+        FrameLayout frameLayout = new FrameLayout(this);
+        frameLayout.setBackgroundResource(R.mipmap.bg);
+        setContentView(frameLayout);
+
+        text1 = new TextView(this);
+        text1.setText("Start Game");
+        text1.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+        text1.setTextColor(Color.rgb(17,85,114));
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+        params.gravity = Gravity.CENTER;
+        text1.setLayoutParams(params);
+
+        text1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("Attention")
+                        .setMessage("You might be killed in this game")
+                        .setPositiveButton("Yes",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Log.i("Game","Entered");
+                                    }
+                                })
+                        .setNegativeButton("No",
+                                new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Log.i("Game","Exited");
+                                        finish();
+                                    }
+                                }).show();
+            }
+        });
+
+        frameLayout.addView(text1);
     }
 }
